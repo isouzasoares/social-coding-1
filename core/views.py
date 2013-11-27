b51b1ab653ec867e649f79b7623f8d4bbebaec51
Raphael <raphapassini@gmail.com>
2013-11-27 20:12:45 -0200

    Login into foursquare is done

--- core/views.py
+++ core/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import foursquare
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from project import settings
 
-# Create your views here.
+
+def fq_login(request):
+    client = _create_fq_client()
+    auth_uri = client.oauth.auth_url()
+    return redirect(auth_uri)
+
+
+def fq_auth(request):
+    client = _create_fq_client()
+    code = request.GET.get('code')
+    access_token = client.oauth.get_token(code)
+    client.set_access_token(access_token)
+
+    # Get the user's data
+    user = client.users()
+    return HttpResponse('Usuario logado %s' % (user['user']['id'], ))
+
+
+def _create_fq_client():
+    return foursquare.Foursquare(
+        client_id=settings.FOURSQUARE_CLIENTID,
+        client_secret=settings.FOURSQUARE_SECRET,
+        redirect_uri=settings.FOURSQUARE_REDIRECTURI)
--- project/urls.py
+++ project/urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
+    url(r'^$', 'core.views.fq_login', name='fq_login'),
+    url(r'fq/.*', 'core.views.fq_auth', name='fq_auth'),
     url(r'^admin/', include(admin.site.urls)),
 )

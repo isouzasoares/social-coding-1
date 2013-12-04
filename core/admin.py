f916970c97f3657901826056a3019b3e4e43c4f1
Igor P. Leroy <ip.leroy@gmail.com>
2013-12-04 19:31:34 -0200

    Adds default admin for core apps

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from core.models import Consumer, Place, Rating
 
-# Register your models here.
+
+class ConsumerAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Consumer, ConsumerAdmin)
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Place, PlaceAdmin)
+
+
+class RatingAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Rating, RatingAdmin)
